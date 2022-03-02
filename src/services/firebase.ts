42f86a6bbc1ea278368d9c604b5e53e905c961f5
Dev-Paulo-Henrique <jwcfree11@gmail.com>
2022-03-02 15:28:41 -0300

    Repository

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,13 +4,13 @@ import "firebase/auth";
 import "firebase/database";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBkp0dmP5jLcvPwgMQYNpIK6BmBIGmfaQo",
+  authDomain: "chat-9bd07.firebaseapp.com",
+  databaseURL: "https://chat-9bd07.firebaseio.com",
+  projectId: "chat-9bd07",
+  storageBucket: "chat-9bd07.appspot.com",
+  messagingSenderId: "1096211615371",
+  appId: "1:1096211615371:web:cdc5492dcfb9805788ce8e",
 };
 
 if (!firebase.apps.length) {
