1b9140fa7b4a004a032d4da4d31ce0f86aa004c7
Paulo Henrique <jwcfree11@gmail.com>
2022-03-05 15:36:54 -0300

    Update getState.js

--- src/pages/rooms/getState.js
+++ src/pages/rooms/getState.js
@@ -1,4 +1,4 @@
-export function getState(){
+export default function getState(){
     const getTeam = document.getElementById('select').value
     const session = sessionStorage.setItem('new', getTeam)
     console.log(sessionStorage.getItem('new'))
