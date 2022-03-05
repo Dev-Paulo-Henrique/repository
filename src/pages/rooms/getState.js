c2e5b0329dc424e1226c04612820d7ddf74555d4
Paulo Henrique <jwcfree11@gmail.com>
2022-03-05 16:37:36 -0300

    Delete getState.js

--- src/pages/rooms/getState.js
+++
@@ -1,5 +0,0 @@
-export default function getState(){
-    const getTeam = document.getElementById('select').value
-    const session = sessionStorage.setItem('new', getTeam)
-    console.log(sessionStorage.getItem('new'))
-}
