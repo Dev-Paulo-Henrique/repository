be6a16606a978f15103dde9c1f49c2969a4830a1
Paulo Henrique <jwcfree11@gmail.com>
2023-01-23 19:42:04 -0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ import "../assets/styles/global.scss";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
+  console.log(router, router.pathname)
   return (
     <AuthProvider>
       <Component {...pageProps} />
