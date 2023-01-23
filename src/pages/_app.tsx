4add0780266f6703b28d57cd39837850b2e25f16
Paulo Henrique <jwcfree11@gmail.com>
2023-01-23 19:33:59 -0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,17 @@
 import { Toaster } from "react-hot-toast";
 import { AuthProvider } from "../contexts/AuthContext";
 import DarkModeSwitch from "../components/DarkModeSwitch";
+import Router from 'next/router';
 
 import "../assets/styles/global.scss";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
   return (
     <AuthProvider>
       <Component {...pageProps} />
       <Toaster position="top-right" />
-      {window.location.href !== "https://repository-neon.vercel.app/rooms/app/-MxAVG-JyrLMdw1xfUZF" && <DarkModeSwitch />}
+      {router.pathname !== "/rooms/app/-MxAVG-JyrLMdw1xfUZF" && <DarkModeSwitch />}
     </AuthProvider>
   );
 }
